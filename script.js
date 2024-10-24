6bbe57b76a5054b305147edd239ed12d142c5c6b
Kodealex <kodeloftet_alexander@hotmail.com>
2024-10-24 11:30:26 +0200

    oppdatering

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello!');
+console.log('Hei på deg hilsen alex');
+
+// variables
+
+// let variable saving the age 35 on the name age
+let age = 35;
+// Chaining method
+console.log('min alder er: ' + age);
+// Template literal
+console.log(`min alder er: ${age}`);
+console.log(typeof age);
+//  const variable saving the string Alexander on the variable name name.
+const name = "Alexander";
+console.log('Mitt navn er: ' + name);
+// Template literal:
+console.log(`mitt navn er: ${name}`);
+console.log('variabel typen er: ', typeof name);
+
+// const variable saving the boolean true on the name isPresent
+const isPresent = true;
+console.log("Er tilstede: ", isPresent);
+// Template literal:
+console.log(`Er tilstedet: ${isPresent}`);
+
+console.log(typeof isPresent);
+
+const firstName = "Alexander";
+const lastName = "Borge";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
